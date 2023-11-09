0546c577f2bb34ede7c1cb0f708c853470ff7d3d
rr <gregrger@gmail.com>
2023-11-09 15:25:47 +0300

    zd14

---
+++ lab 2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+usnig namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int decimalNumber;
+
+    cout << "Введите десятичное число: ";
+    cin >> decimalNumber;
+
+    if (decimalNumber < 0) {
+        cout << "Отрицательные числа не поддерживаются." << endl;
+        return 1;
+    }
+
+    if (decimalNumber == 0) {
+        cout << "Двоичное представление: 0" << endl;
+        return 0;
+    }
+
+    int binary[255]; 
+    int index = 0;
+
+    
+    while (decimalNumber > 0) {
+        binary[index] = decimalNumber % 2;
+        decimalNumber /= 2;
+        index++;
+    }
+
+    cout << "Двоичное представление: ";
+    for (int i = index - 1; i >= 0; i--) {
+        cout << binary[i];
+    }
+    cout << std::endl;
+
+    return 0;
+}
---
+++ lab 4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void printPositiveThenNegative(int arr[], int size) {
+    cout << "Положительные числа: ";
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    cout << "\nОтрицательные числа: ";
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            cout << arr[i] << " ";
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int arr[] = { 44, -21, 190, -6, 9, -43 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printPositiveThenNegative(arr, size);
+
+    return 0;
+}
